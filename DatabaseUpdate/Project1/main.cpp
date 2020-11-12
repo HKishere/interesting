39e6005ee2b26929c55b0fe9b8ef6c803670530e
HKishere <48638533+HKishere@users.noreply.github.com>
2020-11-12 10:07:29 +0800

    修改了程序会在最后崩溃的bug

--- DatabaseUpdate/Project1/main.cpp
+++ DatabaseUpdate/Project1/main.cpp
@@ -9,11 +9,12 @@ int main() {
 
 	//读取文件获取信息
 	ifstream f_inifile;
-	char buff[100];
+	char buff[255];
 	f_inifile.open("HF1.2B_Config.ini");
-
 	if (!f_inifile.is_open()){
-		cout << "配置读取失败!" << endl;
+		cout << "配置打开失败失败!" << endl;
+		system("pause");
+		return 0;
 	}
 
 	string host;
@@ -38,33 +39,54 @@ int main() {
 		if (line.find("port") != -1){
 			port = line.substr(5, line.size());
 		}
+		if (host.size()>0&&user.size()>0&&password.size()>0&&database.size()>0&&port.size()>0)
+		{
+			break;
+		}
 	}
-	//f_inifile.close();
+	if (host.size() > 0 || user.size() > 0 || password.size() > 0 || database.size() > 0 || port.size() > 0)
+	{
+		cout << "配置信息缺失,检查ini文件" << endl;
+		system("pause");
+		return 0;
+	}
+	f_inifile.close();
 
 	MYSQL conn;
 	mysql_init(&conn);
 
+	ifstream f_patched;
 	if (!mysql_real_connect(&conn, host.c_str(), user.c_str(), password.c_str(), database.c_str(), stoi(port), NULL, 0)) {
-		cout << "Failed to connect to database: Error:" << mysql_error(&conn) << endl;
+		cout << "连接数据库失败!" << mysql_error(&conn) << endl;
+		system("pause");
+		return 0;
 	}
 	else {
-		ifstream f_patched;
 		f_patched.open("patch.txt");
 		if (!f_patched.is_open()){
 			cout << "读取更新文件失败." << endl;
+			system("pause");
+			return 0;
 		}
 		int i = 1;
 		while (f_patched.getline(buff,255)){
 			if (mysql_query(&conn, buff)) {//通过mysql_query函数执行SQL语句
+				cout << "第" << i << "条语句执行失败!!请检查SQL语句!" << endl;
 				cout << mysql_error(&conn) << endl;
+				system("pause");
+				return 0;
 			}
 			else{
 				cout << "第" << i << "条语句执行成功" << endl;
 			}
 			i++;
 		}
-		//f_patched.close();
 	}
+	cout << "=======" << endl;
+	cout << "更新成功" << endl;
+	cout << "=======" << endl;
+	f_patched.close();
 	mysql_close(&conn);
 	system("pause");
+	return 0;
 }
